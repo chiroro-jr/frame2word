338c552c7e72baa932b302ca0273d79d28ba5bfb
Nyasha D. Chiroro <chirorodennis@gmail.com>
2023-09-29 00:16:45 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from helpers import get_frames, get_frame_features, get_summary, format_summary
+
+# define the maximum sequence length
+max_length = 34
+
+# target image size expected by feature extractor
+target_size = (224, 224)
+
+"""
+# Frame2Word
+An model that summarizes videos using image captioning. Built using Tensorflow (VGG16 + LSTMs) and
+OpenCV (for reading video frames)
+"""
+
+st.divider()
+
+"""
+## Submit A Video
+"""
+
+video_types = [".mp4", ".mkv", "mov", "m4a", "3gp", "3g2", "mj2"]
+uploaded_video = st.file_uploader(label="Choose a file(less than 2MB)", type=video_types)
+if st.button("Summarize Video", type="secondary"):
+    if uploaded_video is not None:
+        video_size = file_size = len(uploaded_video.read())
+        max_video_size = 2 * 1024 * 1024   # 2MB in bytes
+        # validate the size of the video
+        if video_size <= max_video_size:
+            """
+            ## Generated Summary
+            """
+
+            frames = get_frames(uploaded_video)
+            frame_features = get_frame_features(frames, target_size)
+            summary = get_summary(frame_features, max_length)
+            summary = format_summary(summary)
+            for line in summary.split('\n'):
+                st.write(line)
+        else:
+            """
+            ## Generated Summary
+            You need to submit a video that is equal to or less than 2MB. Summary could not be
+            generated. Try another video.
+            """
+    else:
+        """
+        ## Generated Summary
+        You need to submit a video to get a summary.
+        """
+else:
+    """
+    ## Generated Summary
+    You need to submit a video to get a summary.
+    """
+
+st.divider()
+"""
+## Reference Links
+- [Github repository for project](https://github.com/chiroro-jr/frame2word)
+- [Colab notebook used to train the model]("https://colab.research.google.com/drive/1yt6999Pba9VY1UebsKOxbiZFfjw_vnlc?usp=sharing")
+"""
---
+++ helpers.py
@@ -0,0 +1,136 @@
+import numpy as np
+import importlib
+from keras.preprocessing.image import img_to_array
+from keras.saving import load_model
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.sequence import pad_sequences
+from tempfile import NamedTemporaryFile
+from pickle import load
+
+
+def get_frames(uploaded_video):
+    cv2 = importlib.import_module("cv2")
+
+    # store the uploaded video contents in a temporary file
+    temp_file = NamedTemporaryFile(delete=False)
+    with temp_file as file:
+        file.write(uploaded_video.getvalue())
+
+    # open the temporary video using OpenCV
+    cap = cv2.VideoCapture(temp_file.name)
+
+    # Select 10 frames evenly spaced throughout the video
+    frame_indices = np.linspace(0, cap.get(cv2.CAP_PROP_FRAME_COUNT) - 1, num=2, dtype=int)
+
+    # get 10 frames from the video to use to generate a summary
+    frames = list()
+    for index in frame_indices:
+        # set the frame position
+        cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+
+        # read the frame
+        ret, frame = cap.read()
+
+        if ret:
+            frames.append(frame)
+
+    # release video capture object
+    cap.release()
+    del cv2
+    return frames
+
+
+def index_to_word(provided_idx, tokenizer):
+    for word, idx in tokenizer.word_index.items():
+        if idx == provided_idx:
+            return word
+
+    return None
+
+
+def cleanup_caption(caption: str):
+    # remove the start of sequence token
+    index = caption.find('startseq ')
+    if index > -1:
+        caption = caption[len('startseq'):]
+
+    # remove the end of sequence token
+    index = caption.find(' endseq')
+    if index > -1:
+        caption = caption[:index]
+
+    return caption
+
+
+def generate_caption(frame_features, max_length):
+    # load the image captioning model
+    image_captioner = load_model("./models/image_captioner.h5")
+
+    # load the tokenizer
+    tokenizer = load(open('./models/tokenizer.pkl', 'rb'))
+
+    # seed the generation process
+    in_text = 'startseq'
+    # iterate over the whole length of the sequence
+    for _ in range(max_length):
+        # integer encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad input
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        # predict next word
+        yhat = image_captioner.predict([frame_features, sequence], verbose=0)
+        # convert probability to integer
+        yhat = np.argmax(yhat)
+        # map integer to word
+        word = index_to_word(yhat, tokenizer)
+        # stop if we cannot map the word
+        if word is None:
+            break
+        # append as input for generating the next word
+        in_text += ' ' + word
+        # stop if we predict the end of the sequence
+        if word == 'endseq':
+            break
+
+    del tokenizer
+    del image_captioner
+    return in_text
+
+
+def get_summary(frame_features, max_length):
+    frame_captions = []
+
+    for feature_vector in frame_features:
+        caption = generate_caption(feature_vector, max_length)
+        caption = cleanup_caption(caption)
+        frame_captions.append(caption)
+
+    return frame_captions
+
+
+def get_frame_features(frames, target_size):
+    cv2 = importlib.import_module("cv2")
+
+    # load the feature extraction model
+    model = load_model("./models/feature_extractor.h5")
+
+    features = []
+
+    for frame in frames:
+        # resize the frame to (224, 244) which is the required shape for VGG16
+        frame = cv2.resize(frame, target_size, interpolation=cv2.INTER_LINEAR)
+        frame = img_to_array(frame)
+        frame = frame.reshape((1, frame.shape[0], frame.shape[1], frame.shape[2]))
+        frame = preprocess_input(frame)
+
+        feature_vector = model.predict(frame, verbose=0)
+
+        features.append(feature_vector)
+    del model
+    del cv2
+    return features
+
+
+def format_summary(summary):
+    formatted_sentences = [sentence.strip().capitalize() + '.' if sentence else '' for sentence in summary]
+    return '\n'.join(formatted_sentences)
